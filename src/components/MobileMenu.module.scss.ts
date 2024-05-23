44e0977fe2e8702b21b4c71e373b993e1ecb41cb
SharadDukare6 <sharad.dukare6@gmail.com>
2024-05-23 20:16:39 +0530

    css

--- src/components/MobileMenu.module.scss.ts
+++ src/components/MobileMenu.module.scss.ts
@@ -1,19 +1,26 @@
 /* tslint:disable */
 require("./MobileMenu.module.css");
 const styles = {
-  container: 'container_d63ff6fd',
-  row: 'row_d63ff6fd',
-  togglemenumobile: 'togglemenumobile_d63ff6fd',
-  logomobile: 'logomobile_d63ff6fd',
-  righticonmobile: 'righticonmobile_d63ff6fd',
-  flyoutpanel: 'flyoutpanel_d63ff6fd',
-  'is-active': 'is-active_d63ff6fd',
-  'is-underlined': 'is-underlined_d63ff6fd',
-  menuPanel: 'menuPanel_d63ff6fd',
-  mainMenu: 'mainMenu_d63ff6fd',
-  subMenu: 'subMenu_d63ff6fd',
-  menuItem: 'menuItem_d63ff6fd',
-  subMenuItem: 'subMenuItem_d63ff6fd'
+  container: 'container_d1a5573d',
+  row: 'row_d1a5573d',
+  togglemenumobile: 'togglemenumobile_d1a5573d',
+  logomobile: 'logomobile_d1a5573d',
+  righticonmobile: 'righticonmobile_d1a5573d',
+  flyoutpanel: 'flyoutpanel_d1a5573d',
+  'is-active': 'is-active_d1a5573d',
+  'is-underlined': 'is-underlined_d1a5573d',
+  menuPanel: 'menuPanel_d1a5573d',
+  mainMenu: 'mainMenu_d1a5573d',
+  subsubMenu: 'subsubMenu_d1a5573d',
+  selectedMenuItem: 'selectedMenuItem_d1a5573d',
+  menuItem: 'menuItem_d1a5573d',
+  iconStylefront: 'iconStylefront_d1a5573d',
+  iconStyleback: 'iconStyleback_d1a5573d',
+  subMenuItem: 'subMenuItem_d1a5573d',
+  subMenuItemicon: 'subMenuItemicon_d1a5573d',
+  subMenuItemtext: 'subMenuItemtext_d1a5573d',
+  subsubMenuItem: 'subsubMenuItem_d1a5573d',
+  subMenu: 'subMenu_d1a5573d'
 };
 
 export default styles;
--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -10,10 +10,8 @@ import AppPanel from './AppPanel';
 import { spfi, SPFx } from "@pnp/sp";
 import { TopLevelMenu as TopLevelMenuModel } from '../model/TopLevelMenu';
 import { FlyoutColumn } from '../model/FlyoutColumn';
-import { Link } from '../model/Link'; // Import the Link class
+import { Link } from '../model/Link';
 
-
-//import {WebPartContext} from '@microsoft/sp-webpart-base'
 export interface IMobileMenuProps {
     handleTouched?: () => void;
     responsiveMode?: ResponsiveMode;
@@ -23,35 +21,35 @@ export interface IMobileMenuProps {
 
 export interface IMobileMenuState {
     isMenuOpen: boolean;
-    isFlyoutOpen: boolean;  // State for flyout visibility
+    isFlyoutOpen: boolean;
     isSearchBoxExpanded: boolean;
     isSearchBoxVisible: boolean;
-    openSubMenu: { [key: string]: boolean }; // To keep track of open submenus
+    openSubMenu: { [key: string]: boolean };
+    selectedMenuItem: string | null; // Track the selected menu item
 }
 
 @withResponsiveMode
 export class MobileMenu extends React.Component<IMobileMenuProps, IMobileMenuState> {
     sp: any;
+
+    state: IMobileMenuState = {
+        isMenuOpen: false,
+        isFlyoutOpen: false,
+        isSearchBoxExpanded: false,
+        isSearchBoxVisible: false,
+        openSubMenu: {},
+        selectedMenuItem: null // Initialize with no selected menu item
+    };
+
     constructor(props: IMobileMenuProps) {
-      
         super(props);
         this.sp = spfi().using(SPFx(props.spfxContext));
-        this.state = {
-            isMenuOpen: false,
-            isFlyoutOpen: false,
-            isSearchBoxExpanded: false,
-            isSearchBoxVisible: false,
-            openSubMenu: {}
-        };
     }
 
     toggleMenu = () => {
         this.setState(prevState => ({
             isMenuOpen: !prevState.isMenuOpen
         }));
-        if (this.props.handleTouched) {
-            this.props.handleTouched();
-        }
     };
 
     toggleSubMenu = (menuId: string) => {
@@ -59,34 +57,29 @@ export class MobileMenu extends React.Component<IMobileMenuProps, IMobileMenuSta
             openSubMenu: {
                 ...prevState.openSubMenu,
                 [menuId]: !prevState.openSubMenu[menuId]
-            }
+            },
+            selectedMenuItem: prevState.selectedMenuItem === menuId ? null : menuId // Toggle selected menu item
         }));
     };
-
 
     toggleFlyout = () => {
         this.setState(prevState => ({
-            isFlyoutOpen: !prevState.isFlyoutOpen  // Toggle flyout visibility
+            isFlyoutOpen: !prevState.isFlyoutOpen
         }));
     };
 
-    // This method is called when the search is performed
     onSearch = (searchTerm: string): void => {
-        // Construct the URL for the Microsoft Search results page
-        // Replace 'BIO_WEB_SEARCH_VERTICAL_ID' with your actual search vertical identifier for BioWeb
         const searchVerticalIdentifier = 'BIO_WEB_SEARCH_VERTICAL_ID';
         const searchBaseUrl = `/_layouts/15/search.aspx/siteall`;
 
-        // Construct the query parameter
-        // If you need to search only within sites prefixed with “/sites/BioWeb-“, adjust the query as needed
         const queryParams = new URLSearchParams({
             q: searchTerm,
-            v: searchVerticalIdentifier // This parameter should correspond to the identifier for your custom search vertical
+            v: searchVerticalIdentifier
         });
 
-        // Redirect to the search results page with the search term and vertical identifier
         window.location.href = `${searchBaseUrl}?${queryParams.toString()}`;
     };
+
     toggleSearch = () => {
         this.setState(prevState => ({
             isSearchBoxVisible: !prevState.isSearchBoxVisible,
@@ -101,16 +94,16 @@ export class MobileMenu extends React.Component<IMobileMenuProps, IMobileMenuSta
                 {columns.map((column, columnIndex) => (
                     <li key={`${parentId}-${columnIndex}`}>
                         <div className={styles.subMenuItem} onClick={() => this.toggleSubMenu(`${parentId}-${columnIndex}`)}>
-                            {column.heading ? column.heading.text : 'Submenu'}
                             {column.links && column.links.length > 0 && (
-                                <Icon iconName={openSubMenu[`${parentId}-${columnIndex}`] ? "ChevronUp" : "ChevronDown"} />
+                                <Icon className={styles.subMenuItemicon} iconName={openSubMenu[`${parentId}-${columnIndex}`] ? "ChevronUp" : "ChevronDown"} />
                             )}
+                            <span className={styles.subMenuItemtext}>{column.heading ? column.heading.text : 'Submenu'}</span>
                         </div>
                         {column.links && openSubMenu[`${parentId}-${columnIndex}`] && (
-                            <ul className={styles.subMenu}>
+                            <ul className={styles.subsubMenu}>
                                 {column.links.map((link: Link) => (
                                     <li key={link.text}>
-                                        <div className={styles.subMenuItem}>
+                                        <div className={styles.subsubMenuItem}>
                                             <a href={link.url || '#'} target={link.openInNewTab ? "_blank" : "_self"}>{link.text}</a>
                                         </div>
                                     </li>
@@ -122,22 +115,20 @@ export class MobileMenu extends React.Component<IMobileMenuProps, IMobileMenuSta
             </ul>
         );
     }
-    public render(): React.ReactElement<IMobileMenuProps> {
 
-        const { isMenuOpen,  isFlyoutOpen } = this.state;
+    public render(): React.ReactElement<IMobileMenuProps> {
+        const { isMenuOpen, isFlyoutOpen, isSearchBoxExpanded, selectedMenuItem } = this.state;
         const iconClassName = isMenuOpen ? "ms-Icon ms-Icon--Cancel" : "ms-Icon ms-Icon--GlobalNavButton";
         const homeUrl = "https://bmrn.sharepoint.com/sites/bioweb-home";
         const { topLevelMenuItems } = this.props;
         const iconClassNameFlyout = isFlyoutOpen ? "ms-Icon ms-Icon--ChevronUpSmall" : "ms-Icon ms-Icon--More";
-       
 
-        // Search box or icon based on state
-        const searchElement = this.state.isSearchBoxExpanded ? (
+        const searchElement = isSearchBoxExpanded ? (
             <SearchBox
                 placeholder="Search BioWeb..."
                 onSearch={this.onSearch}
                 styles={{ root: { width: '100%' } }}
-                onBlur={() => this.setState({ isSearchBoxExpanded: false })}// Optionally hide search when it loses focus
+                onBlur={() => this.setState({ isSearchBoxExpanded: false })}
                 underlined={true}
             />
         ) : (
@@ -145,39 +136,36 @@ export class MobileMenu extends React.Component<IMobileMenuProps, IMobileMenuSta
                 iconName="Search"
                 onClick={this.toggleSearch}
                 className={styles1.searchIcon}
-                // Add the following line to handle expanding the search box on click
                 onMouseDown={() => this.setState({ isSearchBoxExpanded: true })}
             />
         );
 
-
-
-
-          return (
+        return (
             <div className={`ms-Grid ${styles.container}`}>
-                <div  className={`ms-Grid-row ${styles.row}`}     >
+                <div className={`ms-Grid-row ${styles.row}`}>
                     <div className={`ms-Grid-col ms-sm1 ${styles.togglemenumobile}`}>
-                        <i className={iconClassName } aria-hidden="true" style={{ cursor: 'pointer' }} onClick={this.toggleMenu} title='Toggle Navigation Pane' />
+                        <i className={iconClassName} aria-hidden="true" style={{ cursor: 'pointer' }} onClick={this.toggleMenu} title='Toggle Navigation Pane' />
                     </div>
-                    <div className={`ms-Grid-col ms-sm7 ${styles.logomobile}`} >
+                    <div className={`ms-Grid-col ms-sm7 ${styles.logomobile}`}>
                         <a href={homeUrl} className={styles1.logoHomeUrL}><img src={require('../common/img/biomarin.svg')} alt="Biomarin" style={{ width: '120px' }} /></a>
                     </div>
-                    <div className={`ms-Grid-col ms-sm4 ${styles.righticonmobile}`} >
+                    <div className={`ms-Grid-col ms-sm4 ${styles.righticonmobile}`}>
                         <i className={iconClassNameFlyout} aria-hidden="true" style={{ cursor: 'pointer' }} onClick={this.toggleFlyout} title='Toggle more' />
-
                     </div>
-
-
                 </div>
                 {isMenuOpen && (
                     <div className={styles.menuPanel}>
                         <ul className={styles.mainMenu}>
                             {topLevelMenuItems.map(item => (
                                 <li key={item.id}>
-                                    <div className={styles.menuItem} onClick={() => this.toggleSubMenu(item.id.toString())}>
+                                    <div
+                                        className={`${styles.menuItem} ${selectedMenuItem === item.id.toString() ? styles.selectedMenuItem : ''}`}
+                                        onClick={() => this.toggleSubMenu(item.id.toString())}
+                                    >
+                                        <Icon iconName="CircleFill" className={styles.iconStylefront} />
                                         {item.text}
-                                        {item.columns && item.columns.length > 0 && (
-                                            <Icon iconName={this.state.openSubMenu[item.id.toString()] ? "ChevronUp" : "ChevronDown"} />
+                                        {selectedMenuItem === item.id.toString() && (
+                                            <Icon iconName="AcceptMedium" className={styles.iconStyleback} />
                                         )}
                                     </div>
                                     {item.columns && this.state.openSubMenu[item.id.toString()] && this.renderSubMenu(item.columns, item.id.toString())}
@@ -186,15 +174,16 @@ export class MobileMenu extends React.Component<IMobileMenuProps, IMobileMenuSta
                         </ul>
                     </div>
                 )}
-                {isFlyoutOpen  && <div className={` ${styles.flyoutpanel}`} >
-                    <div className={`ms-Grid-col ms-sm12 ms-md12 ms-lg4 ${styles1.searchBoxContainer}`}>
-                        {searchElement}
-                        {!this.state.isSearchBoxExpanded && <QuestionMarkIconWithTooltip spfxContext={this.props.spfxContext} />}
-                         {!this.state.isSearchBoxExpanded && <ChatbotIconWithTooltip />}
-                         {!this.state.isSearchBoxExpanded && <AppPanel spfxContext={this.props.spfxContext} />}
+                {isFlyoutOpen && (
+                    <div className={` ${styles.flyoutpanel}`}>
+                        <div className={`ms-Grid-col ms-sm12 ms-md12 ms-lg4 ${styles1.searchBoxContainer}`}>
+                            {searchElement}
+                            {!isSearchBoxExpanded && <QuestionMarkIconWithTooltip spfxContext={this.props.spfxContext} />}
+                            {!isSearchBoxExpanded && <ChatbotIconWithTooltip />}
+                            {!isSearchBoxExpanded && <AppPanel spfxContext={this.props.spfxContext} />}
+                        </div>
                     </div>
-                </div>}
-
+                )}
             </div>
         );
     }
