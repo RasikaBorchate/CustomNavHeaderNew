755a27c0b6b62beec5317965e7eab58e47b3d5e1
Kuz <andrew.kuz94@gmail.com>
2024-06-04 19:54:49 +0300

    update

--- src/components/MegaMenu.tsx
+++ src/components/MegaMenu.tsx
@@ -11,7 +11,7 @@ import AppPanel from './AppPanel';
 import { Search20Regular } from '@fluentui/react-icons';
 import { spfi, SPFx } from "@pnp/sp";
 import AlertBar from './AlertBar';
-import { FaSearch, FaTimes } from 'react-icons/fa';
+// import { FaSearch, FaTimes } from 'react-icons/fa';
 
 export interface IMegaMenuProps {
     topLevelMenuItems: TopLevelMenuModel[];
@@ -133,20 +133,24 @@ export class MegaMenu extends React.Component<IMegaMenuProps, IMegaMenuState> {
         ));
 
         const searchElement = isSearchBoxExpanded ? (
-            <div style={{ display: 'flex', alignItems: 'center', width: '100%', borderBottom: '1px solid #000' }}>
-               <button type="button" onClick={this.handleClearSearch} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
-                    <FaTimes />
+            <div className={styles.searchMenu}>
+               <button type="button" className={styles.searchMenuClose} onClick={this.handleClearSearch}>
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.837967 1.07354L0.924757 0.969679C1.1851 0.709324 1.58924 0.680404 1.88155 0.882889L1.98541 0.969679L8.95508 7.9395L15.9247 0.969679C16.2176 0.676774 16.6925 0.676774 16.9855 0.969679C17.2783 1.26257 17.2783 1.73744 16.9855 2.03033L10.0156 9L16.9855 15.9696C17.2457 16.23 17.2747 16.6341 17.0722 16.9265L16.9855 17.0304C16.7251 17.2907 16.321 17.3196 16.0286 17.1171L15.9247 17.0304L8.95508 10.0605L1.98541 17.0304C1.69252 17.3232 1.21765 17.3232 0.924757 17.0304C0.631852 16.7375 0.631852 16.2626 0.924757 15.9696L7.89458 9L0.924757 2.03033C0.664402 1.76999 0.635482 1.36584 0.837967 1.07354Z" fill="#333333"/>
+                    </svg>
                 </button>
                 <input
                     type='text'
                     value={searchQuery}
                     onChange={this.handleInputChange}
                     placeholder="Search BioWeb..."
-                    style={{ width: '100%', padding: '8px', border: 'none', outline: 'none' }}
+                    style={{padding: '8px', border: 'none', outline: 'none' }}
                   //  onBlur={() => this.setState({ isSearchBoxExpanded: false })}
                 />
-                <button type="button" onClick={this.handleSearchClick} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
-                    <FaSearch />
+                <button type="button" className={styles.searchMenuButton} onClick={this.handleSearchClick}>
+                    <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.2358 0.1875C15.877 0.1875 20.4501 4.76059 20.4501 10.4018C20.4501 12.8881 19.5617 15.1669 18.0851 16.9382L25.7495 24.6023C26.1121 24.965 26.1121 25.5529 25.7495 25.9155C25.4272 26.2379 24.9268 26.2737 24.5649 26.023L24.4363 25.9155L16.7722 18.2511C15.0009 19.7277 12.7221 20.6161 10.2358 20.6161C4.59458 20.6161 0.0214844 16.043 0.0214844 10.4018C0.0214844 4.76059 4.59458 0.1875 10.2358 0.1875ZM10.2358 2.04464C5.62025 2.04464 1.87863 5.78626 1.87863 10.4018C1.87863 15.0173 5.62025 18.7589 10.2358 18.7589C14.8513 18.7589 18.5929 15.0173 18.5929 10.4018C18.5929 5.78626 14.8513 2.04464 10.2358 2.04464Z" fill="#212121"/>
+                    </svg>
                 </button>
                 
             </div>
